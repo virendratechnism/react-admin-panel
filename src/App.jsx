625e740e36c6004a00225ca5cff9ce6258157a6d
Jaydeep Suthar <technicaljade@gmail.com>
2022-04-05 09:55:29 +0530

    Login Form Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Outlet,
+	Route,
+	Routes,
+	useNavigate,
+} from "react-router-dom";
 import SideBar from "./component/SidebarApp";
-import swal from 'sweetalert';
-import { Card, Table } from 'react-bootstrap';
-import React from 'react';
-import ChartView from './component/ChartView';
-import MyCalendar from './component/MyCalendar';
-import TableView from './component/TableView';
-import ProfilePage from './component/ProfilePage';
-import logo from './image/logo.png';
-import { Button, Col, Form, FormControl, InputGroup, Row } from "react-bootstrap";
+import swal from "sweetalert";
+import { Card, Table } from "react-bootstrap";
+import React from "react";
+import ChartView from "./component/ChartView";
+import MyCalendar from "./component/MyCalendar";
+import TableView from "./component/TableView";
+import ProfilePage from "./component/ProfilePage";
+import logo from "./image/logo.png";
+import {
+	Button,
+	Col,
+	Form,
+	FormControl,
+	InputGroup,
+	Row,
+} from "react-bootstrap";
 
 import { show, hide } from "./store";
 import { useDispatch, useSelector } from "react-redux";
 
+// * Pages Import
+import LoginPage from "./pages/Login/Login";
 
 const App = () => {
 	const openSweat = useSelector((state) => state.alertBox.openSweat);
@@ -36,19 +51,25 @@ const App = () => {
 					});
 					dispatch(hide());
 				}, 5000)}
-			
+
 			<RoutingHandler />
 		</>
 	);
 };
 
+const SidebarLayout = () => (
+	<>
+		<SideBar />
+		<Outlet />
+	</>
+);
+
 const RoutingHandler = () => {
 	return (
 		<>
 			<Router>
-				<SideBar >
-					<Routes>
-						<Route path="/" element={<Home />} />
+				<Routes>
+					<Route element={<SidebarLayout />}>
 						<Route path="/home" element={<Home />} />
 						<Route path="/sweetAlert" element={<SweetAlert />} />
 						<Route path="/sortTable" element={<SortTable />} />
@@ -58,13 +79,21 @@ const RoutingHandler = () => {
 						<Route path="/profile" element={<Profile />} />
 						<Route path="/2fa" element={<Twofa />} />
 						<Route path="/billing" element={<Billing />} />
-						<Route path="/settings/profile" element={<SettingsProfile />} />
+						<Route
+							path="/settings/profile"
+							element={<SettingsProfile />}
+						/>
 						<Route path="/settings/2fa" element={<Settings2fa />} />
-						<Route path="/settings/billing" element={<SettingsBilling />} />
-						{/* <Route path="/logOut" element={<Logout />} /> */}
-						<Route path="*" element={<Error />} />
-					</Routes>
-				</SideBar>
+						<Route
+							path="/settings/billing"
+							element={<SettingsBilling />}
+						/>
+					</Route>
+
+					{/* <Route path="/logOut" element={<Logout />} /> */}
+					<Route index element={<LoginPage />} />
+					<Route path="*" element={<Error />} />
+				</Routes>
 			</Router>
 		</>
 	);
@@ -74,47 +103,56 @@ const Home = () => {
 		<>
 			<h1>Home Page</h1>
 			<div className="">
-				<table className='my-3 mx-5 ' style={{ border: '1px solid' }}>
+				<table className="my-3 mx-5 " style={{ border: "1px solid" }}>
 					{/* <div className='watermark'>virendra katariya</div> */}
 					{/* <img src={logo} style={{opacity: 0.25;}}/> */}
-					<tr style={{ border: '1px solid' }}>
-						<th style={{ border: '1px solid' }}>Company</th>
-						<th style={{ border: '1px solid' }}>Contact</th>
-						<th style={{ border: '1px solid' }}>Country</th>
+					<tr style={{ border: "1px solid" }}>
+						<th style={{ border: "1px solid" }}>Company</th>
+						<th style={{ border: "1px solid" }}>Contact</th>
+						<th style={{ border: "1px solid" }}>Country</th>
 					</tr>
-					<tr style={{ border: '1px solid' }}>
-						<td style={{ border: '1px solid' }}>Alfreds Futterkiste</td>
-						<td style={{ border: '1px solid' }}>Maria Anders</td>
-						<td style={{ border: '1px solid' }}>Germany</td>
+					<tr style={{ border: "1px solid" }}>
+						<td style={{ border: "1px solid" }}>
+							Alfreds Futterkiste
+						</td>
+						<td style={{ border: "1px solid" }}>Maria Anders</td>
+						<td style={{ border: "1px solid" }}>Germany</td>
 					</tr>
-					<tr style={{ border: '1px solid' }}>
-						<td style={{ border: '1px solid' }}>Centro comercial Moctezuma</td>
-						<td style={{ border: '1px solid' }}>Francisco Chang</td>
-						<td style={{ border: '1px solid' }}>Mexico</td>
+					<tr style={{ border: "1px solid" }}>
+						<td style={{ border: "1px solid" }}>
+							Centro comercial Moctezuma
+						</td>
+						<td style={{ border: "1px solid" }}>Francisco Chang</td>
+						<td style={{ border: "1px solid" }}>Mexico</td>
 					</tr>
-					<tr style={{ border: '1px solid' }}>
-						<td style={{ border: '1px solid' }}>Ernst Handel</td>
-						<td style={{ border: '1px solid' }}>Roland Mendel</td>
-						<td style={{ border: '1px solid' }}>Austria</td>
+					<tr style={{ border: "1px solid" }}>
+						<td style={{ border: "1px solid" }}>Ernst Handel</td>
+						<td style={{ border: "1px solid" }}>Roland Mendel</td>
+						<td style={{ border: "1px solid" }}>Austria</td>
 					</tr>
-					<tr style={{ border: '1px solid' }}>
-						<td style={{ border: '1px solid' }}>Island Trading</td>
-						<td style={{ border: '1px solid' }}>Helen Bennett</td>
-						<td style={{ border: '1px solid' }}>UK</td>
+					<tr style={{ border: "1px solid" }}>
+						<td style={{ border: "1px solid" }}>Island Trading</td>
+						<td style={{ border: "1px solid" }}>Helen Bennett</td>
+						<td style={{ border: "1px solid" }}>UK</td>
 					</tr>
-					<tr style={{ border: '1px solid' }}>
-						<td style={{ border: '1px solid' }}>Laughing Bacchus Winecellars</td>
-						<td style={{ border: '1px solid' }}>Yoshi Tannamuri</td>
-						<td style={{ border: '1px solid' }}>Canada</td>
+					<tr style={{ border: "1px solid" }}>
+						<td style={{ border: "1px solid" }}>
+							Laughing Bacchus Winecellars
+						</td>
+						<td style={{ border: "1px solid" }}>Yoshi Tannamuri</td>
+						<td style={{ border: "1px solid" }}>Canada</td>
 					</tr>
-					<tr style={{ border: '1px solid' }}>
-						<td style={{ border: '1px solid' }}>Magazzini Alimentari Riuniti</td>
-						<td style={{ border: '1px solid' }}>Giovanni Rovelli</td>
-						<td style={{ border: '1px solid' }}>Italy</td>
+					<tr style={{ border: "1px solid" }}>
+						<td style={{ border: "1px solid" }}>
+							Magazzini Alimentari Riuniti
+						</td>
+						<td style={{ border: "1px solid" }}>
+							Giovanni Rovelli
+						</td>
+						<td style={{ border: "1px solid" }}>Italy</td>
 					</tr>
 				</table>
 			</div>
-
 		</>
 	);
 };
@@ -126,28 +164,22 @@ const SweetAlert = () => {
 			<h1>SweetAlert Page</h1>
 			{/* <p>User Page</p> */}
 			<button
-				className='btn btn-success my-3 mx-5'
-				onClick={
-					() =>
-						swal({
-							title: 'Order no Dispatched',
-							text: "Order No is : XXX",
-							button: true,
-							buttonText: "Action",
-							dangerMode: true,
-							closeOnClickOutside: false,
-							icon: "warning"
-						}).then(
-							() =>
-								history.push('/')
-						)
-				}>
+				className="btn btn-success my-3 mx-5"
+				onClick={() =>
+					swal({
+						title: "Order no Dispatched",
+						text: "Order No is : XXX",
+						button: true,
+						buttonText: "Action",
+						dangerMode: true,
+						closeOnClickOutside: false,
+						icon: "warning",
+					}).then(() => history.push("/"))
+				}
+			>
 				Sweet Alert
 			</button>
-			<button
-				className="btn btn-danger"
-				onClick={() => dispatch(show())}
-			>
+			<button className="btn btn-danger" onClick={() => dispatch(show())}>
 				Any page render Alert box
 			</button>
 		</>
@@ -160,7 +192,14 @@ const About = () => {
 		<>
 			<h1>About Page</h1>
 			{/* <p>About Page</p> */}
-			<Card className='shadow my-3 mx-5' style={{ borderTop: "3px solid #f39c12", padding: '20px 15px', maxWidth: '25em' }}>
+			<Card
+				className="shadow my-3 mx-5"
+				style={{
+					borderTop: "3px solid #f39c12",
+					padding: "20px 15px",
+					maxWidth: "25em",
+				}}
+			>
 				<Table>
 					<thead>
 						<h4>Opening Hours</h4>
@@ -168,27 +207,27 @@ const About = () => {
 					<tbody>
 						<tr>
 							<td>Monday </td>
-							<td> 9:00 AM  To  6:00 PM</td>
+							<td> 9:00 AM To 6:00 PM</td>
 						</tr>
 						<tr>
 							<td>Tuesday</td>
-							<td> 9:00 AM  To  6:00 PM</td>
+							<td> 9:00 AM To 6:00 PM</td>
 						</tr>
 						<tr>
 							<td>Wednesday </td>
-							<td> 9:00 AM  To  6:00 PM</td>
+							<td> 9:00 AM To 6:00 PM</td>
 						</tr>
 						<tr>
 							<td>Thursday </td>
-							<td> 9:00 AM  To  6:00 PM</td>
+							<td> 9:00 AM To 6:00 PM</td>
 						</tr>
 						<tr>
 							<td>Friday </td>
-							<td> 9:00 AM  To  6:00 PM</td>
+							<td> 9:00 AM To 6:00 PM</td>
 						</tr>
 						<tr>
 							<td>Saturday </td>
-							<td> 9:00 AM  To  6:00 PM</td>
+							<td> 9:00 AM To 6:00 PM</td>
 						</tr>
 						<tr>
 							<td>Sunday </td>
@@ -196,7 +235,12 @@ const About = () => {
 						</tr>
 					</tbody>
 					<tfoot>
-						<button className='btn btn-success my-3 mx-5' onClick={() => navigate(-1)}>Back</button>
+						<button
+							className="btn btn-success my-3 mx-5"
+							onClick={() => navigate(-1)}
+						>
+							Back
+						</button>
 					</tfoot>
 				</Table>
 			</Card>
@@ -232,19 +276,20 @@ const Profile = () => {
 		<>
 			<h1>Profile Page</h1>
 			<ProfilePage />
-		</>);
+		</>
+	);
 };
 const Twofa = () => {
 	const [email, setEmail] = useState("example@gmail.com");
 	const [password, setPassword] = useState("");
 	const alertHandler = () => {
 		let object = {
-			'Email': email,
-			'Password' : password
-		}
+			Email: email,
+			Password: password,
+		};
 		// alert('email'= email, 'Password'= password)
-		alert( JSON.stringify(object))
-	}
+		alert(JSON.stringify(object));
+	};
 	return (
 		<>
 			<h1>2fa Page</h1>
@@ -264,12 +309,14 @@ const Twofa = () => {
 				</Form.Group>
 				<Form.Group className="mb-3" controlId="password">
 					<Form.Label>Password</Form.Label>
-					<Form.Control 
+					<Form.Control
 						required
-						type="password" 
-						placeholder="Password here" 
-						onChange={(e) => {setPassword(e.target.value)}} />
-					
+						type="password"
+						placeholder="Password here"
+						onChange={(e) => {
+							setPassword(e.target.value);
+						}}
+					/>
 				</Form.Group>
 				{/* <Form.Group as={Col} md="3" controlId="validationCustom05">
 					<Form.Label>Zip</Form.Label>
@@ -278,7 +325,7 @@ const Twofa = () => {
 						Please provide a valid zip.
 					</Form.Control.Feedback>
 				</Form.Group> */}
-				<Button variant="primary" type="submit" >
+				<Button variant="primary" type="submit">
 					Submit
 				</Button>
 			</Form>
@@ -286,7 +333,11 @@ const Twofa = () => {
 	);
 };
 const Billing = () => {
-	return (<><h1>Billing Page</h1></>);
+	return (
+		<>
+			<h1>Billing Page</h1>
+		</>
+	);
 };
 const SettingsProfile = () => {
 	return (
@@ -294,21 +345,21 @@ const SettingsProfile = () => {
 			<h1>Settings Profile</h1>
 		</>
 	);
-}
+};
 const Settings2fa = () => {
 	return (
 		<>
 			<h1>Settings 2FA</h1>
 		</>
 	);
-}
+};
 const SettingsBilling = () => {
 	return (
 		<>
 			<h1>Settings Billing</h1>
 		</>
 	);
-}
+};
 const Error = () => {
 	return (
 		<>
@@ -317,6 +368,4 @@ const Error = () => {
 	);
 };
 
-
-
-export default App
+export default App;
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useRef } from "react";
+import "./style.scss"; // main file :: Development
+// import "./style.min.css"; // minified file :: Production
+
+import { useNavigate } from "react-router-dom";
+
+import logo from "../../logo.svg";
+
+const LoginPage = () => {
+	const navigate = useNavigate();
+
+	const companyName = "TaxPatra";
+	const passwordInput = useRef(null);
+
+	const togglePassword = (e) => {
+		if (e.target.checked === true) passwordInput.current.type = "text";
+		else passwordInput.current.type = "password";
+	};
+
+	return (
+		<div className="login__page__bg">
+			<section className="login__card">
+				<section className="header">
+					<img className="logo" src={logo} />
+					<h1 className="title">{companyName}</h1>
+				</section>
+
+				<div className="login__form">
+					<form method="POST" className="form" autoComplete="off">
+						<div className="form__input">
+							<label htmlFor="emailInput">Email</label>
+							<input
+								type="email"
+								name="email"
+								id="email"
+								placeholder="your-awesome@mail.com"
+							/>
+						</div>
+						<div className="form__input">
+							<label htmlFor="passwordInput">Password</label>
+							<input
+								type="password"
+								name="password"
+								id="password"
+								placeholder="secret"
+								ref={passwordInput}
+							/>
+						</div>
+						<div id="checkbox__form__input" className="form__input">
+							<input
+								type="checkbox"
+								name="checkbox"
+								id="checkbox"
+								onChange={(e) => togglePassword(e)}
+							/>
+							<label
+								id="showPasswordInput"
+								htmlFor="showPasswordInput"
+							>
+								Show Password
+							</label>
+						</div>
+						<div className="form__input">
+							<button
+								className="login__btn"
+								onClick={(e) => {
+									e.preventDefault();
+									navigate("/home");
+								}}
+							>
+								Login
+							</button>
+						</div>
+					</form>
+				</div>
+			</section>
+		</div>
+	);
+};
+
+export default LoginPage;
